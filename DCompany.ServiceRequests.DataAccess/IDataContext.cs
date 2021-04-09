8e3636be584a549261fa94d5beec424f87d05849
Leonardo Ferreyra <leo.e.ferreyra@gmail.com>
2021-04-09 11:52:06 -0300

    DataAccess project added

---
+++ DCompany.ServiceRequests.DataAccess/IDataContext.cs
@@ -0,0 +1,10 @@
+﻿using DCompany.ServiceRequests.Models;
+using System.Collections.Generic;
+
+namespace DCompany.ServiceRequests.DataAccess
+{
+    public interface IDataContext
+    {
+        List<ServiceRequestModel> ServiceRequests { get; }
+    }
+}
