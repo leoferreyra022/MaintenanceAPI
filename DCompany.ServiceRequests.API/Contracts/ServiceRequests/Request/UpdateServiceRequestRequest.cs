4d03d43e1275ddf13c822d14a1ed06e528f21e04
Leonardo Ferreyra <leo.e.ferreyra@gmail.com>
2021-04-09 12:02:34 -0300

    rm

--- DCompany.ServiceRequests.API/Contracts/ServiceRequests/Request/UpdateServiceRequestRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DCompany.ServiceRequests.API.Contracts.ServiceRequests.Request
-{
-    public class UpdateServiceRequestRequest
-    {
-        public string Code { get; set; }
-        public string Description { get; set; }
-        public string Status { get; set; }
-    }
-}
