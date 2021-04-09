4dfc2e005ad3655218a8a48e2f78112e66e044cc
Leonardo Ferreyra <leo.e.ferreyra@gmail.com>
2021-04-09 12:04:29 -0300

    services added

---
+++ DCompany.ServiceRequests.API/Services/IServiceRequestService.cs
@@ -0,0 +1,16 @@
+﻿using DCompany.ServiceRequests.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DCompany.ServiceRequests.API.Services
+{
+    public interface IServiceRequestService
+    {
+        Task<List<ServiceRequestModel>> GetAllAsync();
+        Task<ServiceRequestModel> GetByIdAsync(Guid id);
+        Task<bool> AddAsync(ServiceRequestModel post);
+        Task<bool> UpdateAsync(ServiceRequestModel post);
+        Task<bool> DeleteByIdAsync(Guid id);
+    }
+}
