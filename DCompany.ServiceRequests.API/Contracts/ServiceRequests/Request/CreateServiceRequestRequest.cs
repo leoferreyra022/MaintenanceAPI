2ce7522e05a020ee50f6432e529e4351a41a7dca
Leonardo Ferreyra <leo.e.ferreyra@gmail.com>
2021-04-09 12:07:06 -0300

    del

--- DCompany.ServiceRequests.API/Contracts/ServiceRequests/Request/CreateServiceRequestRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DCompany.ServiceRequests.API.Contracts.ServiceRequests.Request
-{
-    public class CreateServiceRequestRequest
-    {
-        public string Code { get; set; }
-        public string Description { get; set; }
-    }
-}
